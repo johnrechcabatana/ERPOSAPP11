c6780a23d85c46adf6f00491f261b2286222eb3d
Israil <ipcastino@gmail.com>
2019-11-03 16:52:05 +0800

    New Updates Design and Changes on search

--- 11_app/public/js/headerimgs.js
+++ 11_app/public/js/headerimgs.js
@@ -1,5 +1,20 @@
 $( document ).ready(function() {
  	$('.erpnext-icon').attr('src','assets/11_app/images/eph-logo-png.png'); 
- 	$('.centered.splash img').attr('src','assets/11_app/images/eph-logo-png.png'); 
- 	console.log('Load image');
+ 	$('.centered.splash img').attr('src','assets/11_app/images/eph-logo-png.png');  
+
+ 	var enterPressed = 0;
+	window.onkeypress = function (e) {
+	 var keyCode = (e.keyCode || e.which);
+	 if (keyCode === 13) {
+		 
+		  if (enterPressed === 0) {
+		   enterPressed++;
+		   console.log("Enter pressed once. enterPressed is " + enterPressed);
+		  } else if (enterPressed === 2) {
+		   e.preventDefault(); 
+		     console.log("Enter pressed twice. enterPressed is " + enterPressed); 
+		  }
+	 return;
+	 }
+	};
 });

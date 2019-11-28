88dabc8f4d248117a73d0373d1368c6d0ae9c68e
johndef <cabatana.johnrech.g@gmail.com>
2019-11-28 09:01:12 +0800

    sdfkmsdf

--- 11_app/public/js/sales_order.js
+++ 11_app/public/js/sales_order.js
@@ -12,25 +12,12 @@ cur_frm.cscript.item_code=function(doc, cdt, cdn)
 		{
 			console.log(r.message);
 		    frappe.model.set_value(cdt, cdn, "volume",r.message[0].item_name+","+ r.message[0].volume+" "+(r.message[0].brand_name || ""));
-		    frappe.model.set_value(cdt, cdn, "description", "sales");
+		    frappe.model.set_value(cdt, cdn, "description", r.message[0].description);
 		    frappe.model.set_value(cdt, cdn, "brand_name", r.message[0].brand_name);
 		    // frappe.model.set_value(cdt, cdn, "uom", r.message[0].stock_uom);
 		    stock_uom=r.message[0].stock_uom;
 		}
 	});
-
-	// frappe.call({
-	// 	method:"11_app.script.stock_entry.rate_selling",
-	// 	args:{"item_code":values.item_code,"uom":stock_uom},
-	// 	callback:function(r)
-	// 	{	console.log(stock_uom);
-	// 		console.log(r.message);
-	// 		// if(r.message[0].price_list_rate != null)
-	// 		// {
-	// 		// 	frappe.model.set_value(cdt, cdn, "rate", r.message[0].price_list_rate);
-	// 		// }
-	// 	}
-	// });
 
 }
 cur_frm.cscript.batch_no=function(frm, cdt, cdn)
